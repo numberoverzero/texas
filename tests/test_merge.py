56fe634ddf40dcd1ba7f7bccd0d34c40c7638a6f
Joe Cross <joe.mcross@gmail.com>
2016-02-21 04:02:02 -0800

    Add giant docstring to merger, implement remaining merge conflict resolution

--- tests/test_merge.py
+++ tests/test_merge.py
@@ -7,14 +7,172 @@ def factory():
     return dict
 
 
-def test_merge_scalar(factory):
+def test_missing(factory):
+    contexts = [
+        {
+            "something": "bottom"
+        },
+        {
+            "other": "top"
+        }
+    ]
+    path = "missing"
+    with pytest.raises(KeyError):
+        merge(factory, contexts, path)
+
+
+def test_empty(factory):
+    contexts = [
+        {
+            "something": "bottom"
+        },
+        {
+            "other": {}
+        }
+    ]
+    path = "other"
+    assert merge(factory, contexts, path) == {}
+
+
+def test_scalar(factory):
+    contexts = [
+        {
+            "scalar": "bottom"
+        },
+        {
+            "scalar": "top"
+        }
+    ]
+    path = "scalar"
+    assert merge(factory, contexts, path) == "top"
+
+
+def test_mapping(factory):
+    contexts = [
+        {
+            "dict": {
+                "key": "bottom"
+            }
+        },
+        {
+            "dict": {
+                "key": "top"
+            }
+        }
+    ]
+    path = "dict"
+    expected = {"key": "top"}
+    assert merge(factory, contexts, path) == expected
+
+
+def test_conflicts(factory):
+    """
+    Conflicts:
+        Type: when there is a scalar/mapping conflict, the type
+            of the last context wins.
+        Scalar: when a key has multiple scalar values, the value
+            of the last context wins.
+        Mapping: when a key has multiple mapping values, the mapping values
+            are merged according to the above rules.
+    """
+    contexts = [
+        {
+            "dict": {
+                "key": "bottom",
+                "conflict-scalar": {
+                    "mapping": "bottom"
+                },
+                "conflict-mapping": "scalar"
+            }
+        },
+        {
+            "dict": {
+                "key": "top",
+                "conflict-scalar": "scalar",
+                "conflict-mapping": {
+                    "mapping": "top"
+                }
+            }
+        }
+    ]
+    path = "dict"
+    expected = {
+        "key": "top",
+        "conflict-scalar": "scalar",
+        "conflict-mapping": {
+            "mapping": "top"
+        }
+    }
+    assert merge(factory, contexts, path) == expected
+
+
+def test_complex(factory):
     bottom = {
-        "scalar": "bottom"
+        "shared": {
+            "same": "bottom"
+        },
+        "bottom-only": "bo-value",
+        "multi-mix": {
+            "bottom": "mmb-value"
+        }
+    }
+    middle = {
+        "shared": {
+            "same": "middle",
+            "sm-key": {
+                "smk-key": "smk-value"
+            }
+        },
+        "multi-mix": "mmm-value"
     }
     top = {
-        "scalar": "top"
+        "shared": {
+            "added": "top"
+        },
+        "top-only": {
+            "to-key": "to-value"
+        },
+        "multi-mix": {
+            "top": "mmt-value"
+        }
     }
-    contexts = [bottom, top]
-    path = "scalar"
 
-    assert merge(factory, contexts, path) == "top"
+    expected = {
+        # solo-scalar(bottom)
+        "bottom-only": "bo-value",
+
+        # multi-merge(bottom, middle, top)
+        "shared": {
+
+            # multi-scalar(bottom, middle)
+            "same": "middle",
+
+            # solo-merge(middle)
+            "sm-key": {
+                "smk-key": "smk-value"
+            },
+
+            # solor-scalar(top)
+            "added": "top"
+        },
+
+        # solo-merge(top)
+        "top-only": {
+            "to-key": "to-value"
+        },
+
+        # conflict(bottom, middle, top)
+        "multi-mix": {
+            "top": "mmt-value",
+            "bottom": "mmb-value"
+        }
+    }
+
+    # Wrap the dicts above in a single "key" so we verify the full contexts
+    contexts = [
+        {"key": bottom},
+        {"key": middle},
+        {"key": top}
+    ]
+    path = "key"
+    assert merge(factory, contexts, path) == expected
--- texas/merger.py
+++ texas/merger.py
@@ -1,73 +1,83 @@
 """
-[
-    # Bottom==================================================================
+For the set of contexts [bottom, ..., top]:
+
+The type of each value is determined by the first occurrance (in reverse order)
+to be either a mapping (collections.abc.Mapping) or a scalar (anything else).
+
+If the value is a scalar, the first value (again, in reverse order) is used.
+If the value is a mapping, all other values in contexts that are ALSO mappings
+are merged with the same strategy.  The non-mapping values for that key are
+pruned, since there is no way to resolve them.
+
+Because the type (mapping/scalar) of the first occurrance is used, there is
+never ambiguity when resolving a conflict.
+
+In each of the examples below, the "top" context is below the "bottom" context,
+since the end of a list is the top.
+
+TYPE CONFLICT scalar/mapping: top wins
     {
-        "shared": {
-            "same": "bottom"
-        },
-        "bottom-only": "bo-value",
-        "multi-mix": {
-            "bottom": "mmb-value"
+        "key": "bottom"
+    },
+    {
+        "key": {
+            "mapping": "top"
         }
     }
+    --> {"mapping": "top"}
 
-    # Next ===================================================================
+TYPE CONFLICT scalar/mapping: top wins
     {
-        "shared": {
-            "same": "next",
-            "sn-key": {
-                "snk-key": "snk-value"
-            }
-        },
-        "multi-mix": "mmn-value""
+        "key": {
+            "mapping": "bottom"
+        }
+    },
+    {
+        "key": "top"
     }
+    --> "top"
 
-    # Top ====================================================================
+VALUE CONFLICT scalar/scalar: top wins
     {
-        "shared": {
-            "added": "top"
-        },
-        "top-only": {
-            "to-key": "to-value"
-        },
-        "multi-mix": {
-            "top": "mmt-value"
+        "key": "bottom"
+    },
+    {
+        "key": "top"
+    }
+    --> "top"
+
+VALUE CONFLICT mapping/mapping: merge
+    {
+        "key": {
+            "bottom": "bottom",
+            "conflict": "bottom",
+        }
+    },
+    {
+        "key": {
+            "conflict": "top",
+            "top": "top",
         }
     }
-]
+    --> {"bottom": "bottom", "conflict": "top", "top": "top"}
 
-
-# Combined ===================================================================
-{
-    # solo-scalar(bottom)
-    "bottom-only": "bo-value",
-
-    # multi-merge(bottom, next, top)
-    "shared": {
-
-        # multi-scalar(bottom, next)
-        "same": "next",
-
-        # solo-merge(next)
-        "sn-key": {
-            "snk-key": "snk-value"
-        },
-
-        # solor-scalar(top)
-        "added": "top"
+VALUE CONFLICT mapping/scalar/mapping: merge mappings, drop scalar
+    {
+        "key": {
+            "bottom": "bottom",
+            "conflict": "bottom",
+        }
     },
-
-    # solo-merge(top)
-    "top-only": {
-        "to-key": "to-value"
+    {
+        "key": "scalar"
     },
-
-    # conflict(bottom, next, top)
-    "multi-mix": {
-        "top": "mmt-value",
-        "bottom": "mmb-value"
+    {
+        "key": {
+            "conflict": "top",
+            "top": "top"
+        }
     }
-}
+    --> {"bottom": "bottom", "conflict": "top", "top": "top"}
 """
 import collections.abc
 import enum
@@ -86,18 +96,37 @@ class MergeType(enum.Enum):
 
 
 def get_values(containers, path):
+    """Only returns values from containers that have the 'path' key"""
     return [c[path] for c in containers if path in c]
 
 
+def get_mappings(values):
+    return [v for v in values if isinstance(v, collections.abc.Mapping)]
+
+
 class Resolver:
-    def __init__(self, containers, path, unresolved, output):
+    def __init__(self, factory, unresolved, output, containers, path):
+        # State management.
+
+        # Call to create a new mapping
+        self.factory = factory
+
+        # Append to push new resolvers
+        self.unresolved = unresolved
+
+        # Mapping - set output[path] to store the resolution
+        self.output = output
+
+        # Specific to this resolver, these may be inner dicts.
+        # Path will always be a single string, relative to the current
+        # container.
         self.containers = containers
         self.path = path
-        self.unresolved = unresolved
-        self.output = output
 
     def resolve(self):
         values = get_values(self.containers, self.path)
+        if not values:
+            return
         merge_type = MergeType.resolve(values)
 
         # Scalar - simply return top value.
@@ -105,16 +134,48 @@ class Resolver:
         # this is immediately resolved.
         if merge_type is MergeType.Scalar:
             self.output[self.path] = values[-1]
+            return
+
+        # From here on, we're merging mappings.
+        # First, let's drop all the non-mapping values, since they're
+        # incompatible with the top context's type.
+        values = get_mappings(values)
+
+        # Aside from the new output container, we won't actually
+        # store any new output values.  Instead, we'll push new
+        # Resolvers into the unresolved list, to be processed on the
+        # next iteration.
+        output = self.output[self.path] = self.factory()
+
+        # We don't want to resolve a key twice, so keep track
+        # of which ones have been created
+        seen = set()
+
+        for value in values:
+            for key in value:
+                if key in seen:
+                    continue
+                seen.add(key)
+                # Three things changed from this instance:
+                # self.output -> output (moving one level deeper)
+                # self.containers -> values (inner values to filter)
+                # self.path -> key (key in the next mapping's level)
+                resolver = Resolver(self.factory, self.unresolved,
+                                    output, values, key)
+                self.unresolved.append(resolver)
+
+    def __repr__(self):  # pragma: no cover
+        return "Resolver(path={}, containers={})".format(
+            self.path, self.containers)
 
 
 def merge(factory, contexts, path):
     unresolved = []
     output = factory()
 
-    root_resolver = Resolver(contexts, path, unresolved, output)
+    root_resolver = Resolver(factory, unresolved, output, contexts, path)
     unresolved.append(root_resolver)
 
     while unresolved:
-        resolver = unresolved.pop()
-        resolver.resolve()
+        unresolved.pop().resolve()
     return output[path]
